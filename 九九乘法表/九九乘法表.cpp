8543d618202ea533dc47cc9905c2f5d86b6c8469
Porcelain-vip <Porcelain.vip@qq.com>
2017-03-23 22:07:49 +0800

    九九乘法表

---
+++ 九九乘法表/九九乘法表.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<iomanip>
+
+int main()
+{
+	using namespace std;
+	for (int a = 1, b = 1; a != 10; ++b)
+	{
+		cout << a << "*" << b << "=" << setfill('0') << setw(2) << a*b << "  ";
+		if (a == b)
+		{
+			++a; b = 0; cout << endl;
+		}
+	}
+	return EXIT_SUCCESS;
+}
