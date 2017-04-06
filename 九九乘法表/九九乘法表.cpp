cd0d5473f3bc165ed0cd6dac777f50ebefc3e446
Porcelain-vip <Porcelain.vip@qq.com>
2017-04-06 19:37:05 +0800

    代码优化

--- 九九乘法表/九九乘法表.cpp
+++ 九九乘法表/九九乘法表.cpp
@@ -6,7 +6,7 @@ int main()
 	using namespace std;
 	for (int a = 1, b = 1; a != 10; ++b)
 	{
-		cout << a << "*" << b << "=" << setfill('0') << setw(2) << a*b << "  ";
+		cout << a << "*" << b << "=" << setfill(' ') << setw(2) << a*b << "  ";
 		if (a == b)
 		{
 			++a; b = 0; cout << endl;
